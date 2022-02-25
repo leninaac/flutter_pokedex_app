a40c55028203539950b6b35a277a5125506cc9fa
lenin@ezsolucoes.com.br <lenin@ezsolucoes.com.br>
2022-02-25 09:39:24 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/lenin/pokedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.lenin.pokedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/common/repositories/pokemon_repository.dart';
+
+import 'features/pokedex/route.dart';
+import 'features/pokedex/screens/home/container/home_container.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: PokedexRoute(repository: PokemonRepository(dio: Dio()),),
+    );
+  }
+}
